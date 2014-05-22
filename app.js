c50cefb9bfb95f51f7b8e589c858328e558a509c
Summer <summermichiko@gmail.com>
2014-05-22 14:54:17 -0400

    header restyle

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var headerTop = $('header').offset().top;
-    var headerBottom = headerTop + 600; // Sub-menu should appear after this distance from top.
+    var headerBottom = headerTop + 630; // Sub-menu should appear after this distance from top.
     $(window).scroll(function () {
         var scrollTop = ($(window).scrollTop()); // Current vertical scroll position from the top
         if (scrollTop > headerBottom) { // Check to see if we have scrolled more than headerBottom
