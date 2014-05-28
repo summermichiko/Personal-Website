55530e296846ffeecae94f778c5d073c1e3f5e3f
Summer <summermichiko@gmail.com>
2014-05-28 11:09:51 -0400

    complete media query >480 and <768

--- app.js
+++ app.js
@@ -30,6 +30,18 @@ $(document).ready(function() {
             }
         });
     };
+    if ($(".laptop img").css("width") === "345px") {
+        var headerTop = $('header').offset().top;
+        var headerBottom = headerTop + 420; // Sub-menu should appear after this distance from top.
+        $(window).scroll(function () {
+            var scrollTop = ($(window).scrollTop()); // Current vertical scroll position from the top
+            if (scrollTop > headerBottom) { // Check to see if we have scrolled more than headerBottom
+                $(".menuBar").fadeIn();
+            } else {
+                $(".menuBar").fadeOut();
+            }
+        });
+    };
 
 
 });
