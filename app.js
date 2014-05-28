fc303a17ab0e008f493bfffb373b36a43ac55006
Summer <summermichiko@gmail.com>
2014-05-28 10:44:21 -0400

    js file updated for smallest media query

--- app.js
+++ app.js
@@ -18,4 +18,18 @@ $(document).ready(function() {
         	}, 700);
     	});
 	});
+    if ($(".laptop img").css("width") === "295px") {
+        var headerTop = $('header').offset().top;
+        var headerBottom = headerTop + 415; // Sub-menu should appear after this distance from top.
+        $(window).scroll(function () {
+            var scrollTop = ($(window).scrollTop()); // Current vertical scroll position from the top
+            if (scrollTop > headerBottom) { // Check to see if we have scrolled more than headerBottom
+                $(".menuBar").fadeIn();
+            } else {
+                $(".menuBar").fadeOut();
+            }
+        });
+    };
+
+
 });
