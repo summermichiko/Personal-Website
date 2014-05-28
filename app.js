f5124627384a4be75eb28f64d23ac8947a78c612
Summer <summermichiko@gmail.com>
2014-05-28 15:24:14 -0400

    fixed menuBar fadeIn on scroll down

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-    var headerTop = $('header').offset().top;
-    var headerBottom = headerTop + 670; // Sub-menu should appear after this distance from top.
-    $(window).scroll(function () {
-        var scrollTop = ($(window).scrollTop()); // Current vertical scroll position from the top
-        if (scrollTop > headerBottom) { // Check to see if we have scrolled more than headerBottom
-          	$(".menuBar").fadeIn();
-        } else {
-         	$(".menuBar").fadeOut();
-        }
-    });
     $(function() {
     	$(".navscroll").bind('click',function(event){
     		event.preventDefault();
@@ -44,7 +34,19 @@ $(document).ready(function() {
     };
     if ($(".laptop img").css("width") === "350px") {
         var headerTop = $('header').offset().top;
-        var headerBottom = headerTop + 400; // Sub-menu should appear after this distance from top.
+        var headerBottom = headerTop + 335; // Sub-menu should appear after this distance from top.
+        $(window).scroll(function () {
+            var scrollTop = ($(window).scrollTop()); // Current vertical scroll position from the top
+            if (scrollTop > headerBottom) { // Check to see if we have scrolled more than headerBottom
+                $(".menuBar").fadeIn();
+            } else {
+                $(".menuBar").fadeOut();
+            }
+        });
+    };
+    if ($(".laptop img").css("width") === "335px") {
+        var headerTop = $('header').offset().top;
+        var headerBottom = headerTop + 650; // Sub-menu should appear after this distance from top.
         $(window).scroll(function () {
             var scrollTop = ($(window).scrollTop()); // Current vertical scroll position from the top
             if (scrollTop > headerBottom) { // Check to see if we have scrolled more than headerBottom
