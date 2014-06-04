661a0a82cf35c771160f244105f47c2c7ed6689f
Summer <summermichiko@gmail.com>
2014-06-04 09:53:36 -0400

    jquery height changes

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ $(document).ready(function() {
     };
     if ($("#portfolio img").css("width") === "352px") {
         var headerTop = $('header').offset().top;
-        var headerBottom = headerTop + 630; // Sub-menu should appear after this distance from top.
+        var headerBottom = headerTop + 550; // Sub-menu should appear after this distance from top.
         $(window).scroll(function () {
             var scrollTop = ($(window).scrollTop()); // Current vertical scroll position from the top
             if (scrollTop > headerBottom) { // Check to see if we have scrolled more than headerBottom
