4b7dbbd253a5e504d29b0bf73696c1f639385b7b
Summer <summermichiko@gmail.com>
2014-06-14 23:51:06 -0400

    green background with mystory over video

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
     if ($(".item img").css("width") === "375px") { //largest screen
         var headerTop = $('header').offset().top;
-        var headerBottom = headerTop + 590; // Sub-menu should appear after this distance from top.
+        var headerBottom = (headerTop + 590); // Sub-menu should appear after this distance from top.
         $(window).scroll(function () {
             var scrollTop = ($(window).scrollTop()); // Current vertical scroll position from the top
             if (scrollTop > headerBottom) { // Check to see if we have scrolled more than headerBottom
