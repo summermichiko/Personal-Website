dea1e86e62d91d1db2e09e3bf498a0dee257052b
Summer McDonald <summermichiko@gmail.com>
2015-11-23 23:02:53 -0800

    scroll change

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ $(document).ready(function() {
     	});
 	});
 
+	$(".nameHeader").on("click", function(e) {
+		e.preventDefault();
+		var anchor = $(this);
+    	$('html, body').stop().animate({
+        	scrollTop: $(anchor.attr('href')).offset().top
+    	}, 700);
+	});
+
     $(".menuBar img").on("click", function() {
         $(".responsiveDropDown").toggle();
     });
@@ -16,18 +24,5 @@ $(document).ready(function() {
     $(".responsiveDropDown a").on("click", function() {
         $(".responsiveDropDown").hide();
     });
-
-    /*if ($(".menuBar").css("display") === "none") { //smallest screen
-        var headerTop = $('header').offset().top;
-        var headerBottom = headerTop + 450; // Sub-menu should appear after this distance from top.
-        $(window).scroll(function () {
-            var scrollTop = ($(window).scrollTop()); // Current vertical scroll position from the top
-            if (scrollTop > headerBottom) { // Check to see if we have scrolled more than headerBottom
-                $(".dropDown").fadeIn();
-            } else {
-                $(".dropDown").fadeOut();
-            }
-        });
-    };*/
 
 });
